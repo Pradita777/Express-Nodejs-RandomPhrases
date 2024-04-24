c6cc4ee9fec78a4da9cb2b0971bfc3747b412c06
Pradita777 <andres123prada@gmail.com>
2024-04-24 09:19:04 -0500

    🔨 cambios menores

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const os = require("os")
 const app = express()
 
 const port = 80
@@ -24,8 +24,7 @@ const phrases = [
 app.get('/', (req, res) => {
 
     const number = Math.floor(Math.random() * 7);
-
-    res.send(phrases[number])
+    res.send(phrases[number]+" - Container Id: "+os.hostname())
 
 })
 
